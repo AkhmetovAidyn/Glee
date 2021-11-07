b340370df2624b670d778c7abc9ec3f8fa6c6ba1
uchenik1 <inokbeszhan@gmail.com>
2021-11-07 22:01:12 +0600

    added login

--- app/js/main.js
+++ app/js/main.js
@@ -7,8 +7,16 @@ $(function() {
     autoplaySpeed: 2000
   });
 
-  var mixer = mixitup('.products__grid');
-  var mixer = mixitup('.design__items-content');
+  var containerEl1 = $('[data-ref="container-1"]');
+  var containerEl2 = $('[data-ref="container-2"]');
 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 
 });
