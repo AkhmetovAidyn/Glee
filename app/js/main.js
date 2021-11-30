5c8e9dbeca089d34815704a23cc84e28a1d214f2
uchenik1 <inokbeszhan@gmail.com>
2021-11-30 13:26:32 +0600

    added filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,99 @@
-$(function() {
+$(function () {
+
+  $(".star").rateYo({
+    starWidth: "12px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true
+  });
+
+  var keypressSlider = document.querySelector(".filter-price__slider");
+  var input0 = document.querySelector(".filter-price__input-0");
+  var input1 = document.querySelector(".filter-price__input-1");
+  var inputs = [input0, input1];
+
+  noUiSlider.create(keypressSlider, {
+    start: [100, 500],
+    connect: true,
+    step: 0.1,
+    padding: [0, 40],
+    range: {
+      'min': [0.00],
+      'max': [1000]
+    }
+  });
+
+  /* begin Inputs  */
+
+  /* end Inputs  */
+  keypressSlider.noUiSlider.on("update", function (values, handle) {
+    inputs[handle].value = values[handle];
+
+    /* begin Listen to keypress on the input */
+    function setSliderHandle(i, value) {
+      var r = [null, null];
+      r[i] = value;
+      keypressSlider.noUiSlider.set(r);
+    }
+
+    // Listen to keydown events on the input field.
+    inputs.forEach(function (input, handle) {
+      input.addEventListener("change", function () {
+        setSliderHandle(handle, this.value);
+      });
+
+      input.addEventListener("keydown", function (e) {
+        var values = keypressSlider.noUiSlider.get();
+        var value = Number(values[handle]);
+
+        // [[handle0_down, handle0_up], [handle1_down, handle1_up]]
+        var steps = keypressSlider.noUiSlider.steps();
+
+        // [down, up]
+        var step = steps[handle];
+
+        var position;
+
+        // 13 is enter,
+        // 38 is key up,
+        // 40 is key down.
+        switch (e.which) {
+          case 13:
+            setSliderHandle(handle, this.value);
+            break;
+
+          case 38:
+            // Get step to go increase slider value (up)
+            position = step[1];
+
+            // false = no step is set
+            if (position === false) {
+              position = 1;
+            }
+
+            if (position !== null) {
+              setSliderHandle(handle, value + position);
+            }
+
+            break;
+
+          case 40:
+            position = step[0];
+
+            if (position === false) {
+              position = 1;
+            }
+
+            if (position !== null) {
+              setSliderHandle(handle, value - position);
+            }
+
+            break;
+        }
+      });
+    });
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
@@ -20,3 +115,4 @@ $(function() {
   var mixer2 = mixitup(containerEl2, config);
 
 });
+
